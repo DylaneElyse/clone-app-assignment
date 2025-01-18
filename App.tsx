97c9290c42d3d0a495832d824e93f2d6247aa110
DylaneElyse <dylanecunningham@outlook.com>
2025-01-18 13:08:44 -0700

    Adjust spacing

--- App.tsx
+++ App.tsx
@@ -5,35 +5,42 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.flexRow}>
-        <Image
-          source={require("./assets/images/girl-profile-unsplash.jpg")}
-          style={{ width: 40, height: 40, borderRadius: 50 }}
-        />
-        <Text style={styles.titleText}>Your Library</Text>
-        <Image
-          source={require("./assets/images/search.png")}
-          style={{ width: 30, height: 30 }}
-        />
-        <Image
-          source={require("./assets/images/plus.png")}
-          style={{ width: 30, height: 30 }}
-        />
+      <View style={{ ...styles.flexRow, justifyContent: "space-between" }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={require("./assets/images/girl-profile-unsplash.jpg")}
+            style={{ width: 40, height: 40, borderRadius: 50 }}
+          />
+          <Text style={styles.titleText}>Your Library</Text>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={require("./assets/images/search.png")}
+            style={{ width: 30, height: 30 }}
+          />
+          <Image
+            source={require("./assets/images/plus.png")}
+            style={{ width: 30, height: 30, marginLeft: 10 }}
+          />
+        </View>
       </View>
-      <View style={{ ...styles.flexRow, marginBottom: 20 }}>
+      <View style={{ ...styles.flexRow, marginBottom: 20, marginRight: 20 }}>
         <Text style={styles.bubble}>Playlists</Text>
         <Text style={styles.bubble}>Artists</Text>
         <Text style={styles.bubble}>Albums</Text>
         <Text style={styles.bubble}>Artists</Text>
         <Text style={styles.bubble}>Downloaded</Text>
       </View>
-      <View style={styles.flexRow}>
-        <Image
-          source={require("./assets/images/sort-arrows.png")}
-          style={{ width: 15, height: 15 }}
-        />
-        <Text style={{ color: "white", fontSize: 13 }}>Recents</Text>
-        <Text> </Text>
+      <View style={{ ...styles.flexRow, justifyContent: "space-between" }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={require("./assets/images/sort-arrows.png")}
+            style={{ width: 15, height: 15 }}
+          />
+          <Text style={{ color: "white", fontSize: 13, marginLeft: 5 }}>
+            Recents
+          </Text>
+        </View>
         <Image
           source={require("./assets/images/white-squares.png")}
           style={{ width: 20, height: 20 }}
