800f579b6366cbc24a5cbd55a488eed2c7e94bc5
DylaneElyse <dylanecunningham@outlook.com>
2025-01-18 12:34:16 -0700

    Add bubble line

--- App.tsx
+++ App.tsx
@@ -5,11 +5,42 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={{ flexDirection: "row", justifyContent: "space-around", alignItems: "center", width: "100%" }}>
-        <Image source={require("./assets/images/girl-profile-unsplash.jpg")} style={{ width: 50, height: 50, borderRadius: 50 }} />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "100%",
+          marginBottom: 10,
+        }}
+      >
+        <Image
+          source={require("./assets/images/girl-profile-unsplash.jpg")}
+          style={{ width: 40, height: 40, borderRadius: 50 }}
+        />
         <Text style={styles.titleText}>Your Library</Text>
-        <Image source={require("./assets/images/search.png")} style={{ width: 30, height: 30 }} />
-        <Image source={require("./assets/images/plus.png")} style={{ width: 30, height: 30 }} />
+        <Image
+          source={require("./assets/images/search.png")}
+          style={{ width: 30, height: 30 }}
+        />
+        <Image
+          source={require("./assets/images/plus.png")}
+          style={{ width: 30, height: 30 }}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Text style={styles.bubble}>Playlists</Text>
+        <Text style={styles.bubble}>Artists</Text>
+        <Text style={styles.bubble}>Albums</Text>
+        <Text style={styles.bubble}>Artists</Text>
+        <Text style={styles.bubble}>Downloaded</Text>
       </View>
     </View>
   );
@@ -28,5 +59,15 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
     padding: 20,
+  },
+
+  bubble: {
+    fontSize: 13,
+    fontWeight: "light",
+    color: "white",
+    backgroundColor: "#3A3A3A",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 20,
   },
 });
